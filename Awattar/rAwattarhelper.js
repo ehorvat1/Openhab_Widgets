04fb3d78393b363dd3336a5fafcbd2b1073775f6
ehorvat1 <108073384+ehorvat1@users.noreply.github.com>
2024-02-25 10:54:23 +0100

    fix wrong min times for today and tomorrow
    fix wrong min times for today and tomorrow.
    Times where wrong in range of 4:00 - 19:00 where min price time was shown 1h too soon.

--- Awattar/rAwattarhelper.js
+++ Awattar/rAwattarhelper.js
@@ -1,7 +1,6 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 // OpenHAB Rule DSL file rAwattarhelper.js to support bar chart for Awattar prices
-// Should run in a cron event every 5 to 60 minutes (at least at full hour) e.g. cronExpression: 0 0/5 * * * ? *
-// Supports negative prices
+// Should run in a cron event every 5 to 60 minutes (at least at full hour)
 //
 //
 // see also Github: https://github.com/ehorvat1/Openhab_Widgets/tree/main
@@ -84,21 +83,21 @@ if (price_today_min > (aWATTar_Stundenpreise_Today01_Totalgross.state as Number)
 if (price_today_min > (aWATTar_Stundenpreise_Today02_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today02_Totalgross.state as Number).doubleValue hour_today_min = 2 }
 if (price_today_min > (aWATTar_Stundenpreise_Today03_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today03_Totalgross.state as Number).doubleValue hour_today_min = 3 }
 if (price_today_min > (aWATTar_Stundenpreise_Today04_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today04_Totalgross.state as Number).doubleValue hour_today_min = 4 }
-if (price_today_min > (aWATTar_Stundenpreise_Today05_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today05_Totalgross.state as Number).doubleValue hour_today_min = 4 }
-if (price_today_min > (aWATTar_Stundenpreise_Today06_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today06_Totalgross.state as Number).doubleValue hour_today_min = 5 }
-if (price_today_min > (aWATTar_Stundenpreise_Today07_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today07_Totalgross.state as Number).doubleValue hour_today_min = 6 }
-if (price_today_min > (aWATTar_Stundenpreise_Today08_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today08_Totalgross.state as Number).doubleValue hour_today_min = 7 }
-if (price_today_min > (aWATTar_Stundenpreise_Today09_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today09_Totalgross.state as Number).doubleValue hour_today_min = 8 }
-if (price_today_min > (aWATTar_Stundenpreise_Today10_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today10_Totalgross.state as Number).doubleValue hour_today_min = 9 }
-if (price_today_min > (aWATTar_Stundenpreise_Today11_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today11_Totalgross.state as Number).doubleValue hour_today_min = 10 }
-if (price_today_min > (aWATTar_Stundenpreise_Today12_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today12_Totalgross.state as Number).doubleValue hour_today_min = 11 }
-if (price_today_min > (aWATTar_Stundenpreise_Today13_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today13_Totalgross.state as Number).doubleValue hour_today_min = 12 }
-if (price_today_min > (aWATTar_Stundenpreise_Today14_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today14_Totalgross.state as Number).doubleValue hour_today_min = 13 }
-if (price_today_min > (aWATTar_Stundenpreise_Today15_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today15_Totalgross.state as Number).doubleValue hour_today_min = 14 }
-if (price_today_min > (aWATTar_Stundenpreise_Today16_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today16_Totalgross.state as Number).doubleValue hour_today_min = 15 }
-if (price_today_min > (aWATTar_Stundenpreise_Today17_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today17_Totalgross.state as Number).doubleValue hour_today_min = 16 }
-if (price_today_min > (aWATTar_Stundenpreise_Today18_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today18_Totalgross.state as Number).doubleValue hour_today_min = 17 }
-if (price_today_min > (aWATTar_Stundenpreise_Today19_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today19_Totalgross.state as Number).doubleValue hour_today_min = 18 }
+if (price_today_min > (aWATTar_Stundenpreise_Today05_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today05_Totalgross.state as Number).doubleValue hour_today_min = 5 }
+if (price_today_min > (aWATTar_Stundenpreise_Today06_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today06_Totalgross.state as Number).doubleValue hour_today_min = 6 }
+if (price_today_min > (aWATTar_Stundenpreise_Today07_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today07_Totalgross.state as Number).doubleValue hour_today_min = 7 }
+if (price_today_min > (aWATTar_Stundenpreise_Today08_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today08_Totalgross.state as Number).doubleValue hour_today_min = 8 }
+if (price_today_min > (aWATTar_Stundenpreise_Today09_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today09_Totalgross.state as Number).doubleValue hour_today_min = 9 }
+if (price_today_min > (aWATTar_Stundenpreise_Today10_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today10_Totalgross.state as Number).doubleValue hour_today_min = 10 }
+if (price_today_min > (aWATTar_Stundenpreise_Today11_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today11_Totalgross.state as Number).doubleValue hour_today_min = 11 }
+if (price_today_min > (aWATTar_Stundenpreise_Today12_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today12_Totalgross.state as Number).doubleValue hour_today_min = 12 }
+if (price_today_min > (aWATTar_Stundenpreise_Today13_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today13_Totalgross.state as Number).doubleValue hour_today_min = 13 }
+if (price_today_min > (aWATTar_Stundenpreise_Today14_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today14_Totalgross.state as Number).doubleValue hour_today_min = 14 }
+if (price_today_min > (aWATTar_Stundenpreise_Today15_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today15_Totalgross.state as Number).doubleValue hour_today_min = 15 }
+if (price_today_min > (aWATTar_Stundenpreise_Today16_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today16_Totalgross.state as Number).doubleValue hour_today_min = 16 }
+if (price_today_min > (aWATTar_Stundenpreise_Today17_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today17_Totalgross.state as Number).doubleValue hour_today_min = 17 }
+if (price_today_min > (aWATTar_Stundenpreise_Today18_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today18_Totalgross.state as Number).doubleValue hour_today_min = 18 }
+if (price_today_min > (aWATTar_Stundenpreise_Today19_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today19_Totalgross.state as Number).doubleValue hour_today_min = 19 }
 if (price_today_min > (aWATTar_Stundenpreise_Today20_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today20_Totalgross.state as Number).doubleValue hour_today_min = 20 }
 if (price_today_min > (aWATTar_Stundenpreise_Today21_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today21_Totalgross.state as Number).doubleValue hour_today_min = 21 }
 if (price_today_min > (aWATTar_Stundenpreise_Today22_Totalgross.state as Number)) { price_today_min = (aWATTar_Stundenpreise_Today22_Totalgross.state as Number).doubleValue hour_today_min = 22 }
@@ -157,21 +156,21 @@ price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow00_Totalgross.state as Numbe
   if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow02_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow02_Totalgross.state as Number).doubleValue hour_tomorrow_min = 2 }
   if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow03_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow03_Totalgross.state as Number).doubleValue hour_tomorrow_min = 3 }
   if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow04_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow04_Totalgross.state as Number).doubleValue hour_tomorrow_min = 4 }
-  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow05_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow05_Totalgross.state as Number).doubleValue hour_tomorrow_min = 4 }
-  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow06_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow06_Totalgross.state as Number).doubleValue hour_tomorrow_min = 5 }
-  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow07_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow07_Totalgross.state as Number).doubleValue hour_tomorrow_min = 6 }
-  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow08_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow08_Totalgross.state as Number).doubleValue hour_tomorrow_min = 7 }
-  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow09_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow09_Totalgross.state as Number).doubleValue hour_tomorrow_min = 8 }
-  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow10_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow10_Totalgross.state as Number).doubleValue hour_tomorrow_min = 9 }
-  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow11_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow11_Totalgross.state as Number).doubleValue hour_tomorrow_min = 10 }
-  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow12_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow12_Totalgross.state as Number).doubleValue hour_tomorrow_min = 11 }
-  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow13_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow13_Totalgross.state as Number).doubleValue hour_tomorrow_min = 12 }
-  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow14_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow14_Totalgross.state as Number).doubleValue hour_tomorrow_min = 13 }
-  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow15_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow15_Totalgross.state as Number).doubleValue hour_tomorrow_min = 14 }
-  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow16_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow16_Totalgross.state as Number).doubleValue hour_tomorrow_min = 15 }
-  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow17_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow17_Totalgross.state as Number).doubleValue hour_tomorrow_min = 16 }
-  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow18_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow18_Totalgross.state as Number).doubleValue hour_tomorrow_min = 17 }
-  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow19_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow19_Totalgross.state as Number).doubleValue hour_tomorrow_min = 18 }
+  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow05_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow05_Totalgross.state as Number).doubleValue hour_tomorrow_min = 5 }
+  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow06_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow06_Totalgross.state as Number).doubleValue hour_tomorrow_min = 6 }
+  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow07_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow07_Totalgross.state as Number).doubleValue hour_tomorrow_min = 7 }
+  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow08_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow08_Totalgross.state as Number).doubleValue hour_tomorrow_min = 8 }
+  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow09_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow09_Totalgross.state as Number).doubleValue hour_tomorrow_min = 9 }
+  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow10_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow10_Totalgross.state as Number).doubleValue hour_tomorrow_min = 10 }
+  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow11_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow11_Totalgross.state as Number).doubleValue hour_tomorrow_min = 11 }
+  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow12_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow12_Totalgross.state as Number).doubleValue hour_tomorrow_min = 12 }
+  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow13_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow13_Totalgross.state as Number).doubleValue hour_tomorrow_min = 13 }
+  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow14_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow14_Totalgross.state as Number).doubleValue hour_tomorrow_min = 14 }
+  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow15_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow15_Totalgross.state as Number).doubleValue hour_tomorrow_min = 15 }
+  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow16_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow16_Totalgross.state as Number).doubleValue hour_tomorrow_min = 16 }
+  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow17_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow17_Totalgross.state as Number).doubleValue hour_tomorrow_min = 17 }
+  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow18_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow18_Totalgross.state as Number).doubleValue hour_tomorrow_min = 18 }
+  if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow19_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow19_Totalgross.state as Number).doubleValue hour_tomorrow_min = 19 }
   if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow20_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow20_Totalgross.state as Number).doubleValue hour_tomorrow_min = 20 }
   if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow21_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow21_Totalgross.state as Number).doubleValue hour_tomorrow_min = 21 }
   if (price_tomorrow_min > (aWATTar_Stundenpreise_Tomorrow22_Totalgross.state as Number)) { price_tomorrow_min = (aWATTar_Stundenpreise_Tomorrow22_Totalgross.state as Number).doubleValue hour_tomorrow_min = 22 }
